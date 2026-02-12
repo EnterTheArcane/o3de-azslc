bdd6659b707da96820cc43b06ff3f3f33137dd42
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-02-12 12:32:10 -0700

    Add multi-platform libc6

--- Platform/Linux/RunnerSetup.ps1
+++ Platform/Linux/RunnerSetup.ps1
@@ -1,13 +1,18 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 
+Write-Host "Add target architectures"
+sudo dpkg --add-architecture amd64
+sudo dpkg --add-architecture arm64
+
 Write-Host "Updating package lists"
 sudo apt-get update
 
 Write-Host "Installing cross-compilation dependencies"
 sudo apt-get install -y --no-install-recommends `
     gcc-aarch64-linux-gnu g++-aarch64-linux-gnu `
-    gcc-x86-64-linux-gnu g++-x86-64-linux-gnu
+    gcc-x86-64-linux-gnu g++-x86-64-linux-gnu `
+    libc6:amd64 libc6:arm64
 
 Write-Host "Installing emulation dependencies"
 sudo apt-get install -y --no-install-recommends `
