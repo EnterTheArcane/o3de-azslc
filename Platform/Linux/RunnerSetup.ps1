b132f8a1ce1de8e2635c5792e6276d5b872858f3
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-02-12 13:19:25 -0700

    Add libstdc++6

--- Platform/Linux/RunnerSetup.ps1
+++ Platform/Linux/RunnerSetup.ps1
@@ -7,54 +7,17 @@ if (-not $IsLinux)
     throw "This script only supports Linux hosts"
 }
 
-Write-Host "Add target architectures"
-dpkg --add-architecture amd64
-dpkg --add-architecture arm64
-
-Write-Host "Configure apt sources"
-@"
-Types: deb
-URIs: http://archive.ubuntu.com/ubuntu/
-Suites: noble
-Components: main restricted universe
-Architectures: amd64
-
-Types: deb
-URIs: http://security.ubuntu.com/ubuntu/
-Suites: noble-security
-Components: main restricted universe
-Architectures: amd64
-
-Types: deb
-URIs: http://archive.ubuntu.com/ubuntu/
-Suites: noble-updates
-Components: main restricted universe
-Architectures: amd64
-
-Types: deb
-URIs: http://azure.ports.ubuntu.com/ubuntu-ports/
-Suites: noble
-Components: main restricted multiverse universe
-Architectures: arm64
-
-Types: deb
-URIs: http://azure.ports.ubuntu.com/ubuntu-ports/
-Suites: noble-updates
-Components: main restricted multiverse universe
-Architectures: arm64
-"@ | Out-File -FilePath /etc/apt/sources.list.d/ubuntu.sources -Encoding utf8
-
 Write-Host "Updating package lists"
-apt-get update
+sudo apt-get update
 
 Write-Host "Installing cross-compilation dependencies"
-apt-get install -y --no-install-recommends `
+sudo apt-get install -y --no-install-recommends `
     gcc-aarch64-linux-gnu g++-aarch64-linux-gnu `
     gcc-x86-64-linux-gnu g++-x86-64-linux-gnu `
-    libc6-dev-amd64-cross libgcc-14-dev-amd64-cross libstdc++-14-dev-amd64-cross `
-    libc6-dev-arm64-cross libgcc-14-dev-arm64-cross libstdc++-14-dev-arm64-cross `
+    libc6-dev-amd64-cross libgcc-14-dev-amd64-cross libstdc++-14-dev-amd64-cross libstdc++6-amd64-cross `
+    libc6-dev-arm64-cross libgcc-14-dev-arm64-cross libstdc++-14-dev-arm64-cross libstdc++6-arm64-cross `
     libc6:amd64 libc6:arm64
 
 Write-Host "Installing emulation dependencies"
-apt-get install -y --no-install-recommends `
+sudo apt-get install -y --no-install-recommends `
     binfmt-support qemu-user-static
