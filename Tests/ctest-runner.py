056d89ad68cc90a0790a2966ab5a577970556fcb
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-02-23 19:04:46 -0700

    Cleanup ctest config

--- Tests/ctest-runner.py
+++ Tests/ctest-runner.py
@@ -5,7 +5,9 @@ Copyright (c) Contributors to the Open 3D Engine Project.
 For complete copyright and license terms please see the LICENSE at the root of this distribution.
 
 SPDX-License-Identifier: Apache-2.0 OR MIT
+"""
 
+"""
 CTest-compatible test runner for AZSLC tests.
 
 Wraps the existing test infrastructure so individual tests can be
